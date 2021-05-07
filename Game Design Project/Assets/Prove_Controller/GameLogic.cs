6a9ce1f81858b30c052d42ac559e3552e58d8ff8
Samuello <samuele.mannala@gmail.com>
2021-05-07 16:40:24 +0200

    AirConsole
    Import Air Console. Prove controller

---
+++ Game Design Project/Assets/Prove_Controller/GameLogic.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NDream.AirConsole;
+using Newtonsoft.Json.Linq;
+
+public class GameLogic : MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log("started");
+    }
+     void Awake()
+    {
+        AirConsole.instance.onMessage += OnMessage;
+    }
+
+    void OnMessage(int fromDeviceID, JToken data)
+    {
+        Debug.Log("message from" + fromDeviceID + ", data:" + data);
+        if(data ["action"] != null && data ["action"].ToString ().Equals("interact"))
+        {
+            Camera.main.backgroundColor = new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f));
+        }
+    }
+
+     void OnDestroy()
+    {
+        //unregister event
+        if(AirConsole.instance != null)
+        {
+            AirConsole.instance.onMessage -= OnMessage;
+        }
+    }
+
+}
