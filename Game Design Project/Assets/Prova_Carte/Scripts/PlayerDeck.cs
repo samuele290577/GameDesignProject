efb0756672372cf68cd682f03655c60937d605be
Gabriele Campanella <gabriele.campanella99@gmail.com>
2021-05-08 10:59:56 +0200

    Progress Bar Deck

--- Game Design Project/Assets/Prova_Carte/Scripts/PlayerDeck.cs
+++ Game Design Project/Assets/Prova_Carte/Scripts/PlayerDeck.cs
@@ -1,10 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerDeck : MonoBehaviour
 {
     public List<Card> deck = new List<Card>();
+    public double size = 0;
+    public Text sizeText;
+    public Image image;
+    public Button warButton;
 
     public void AddCard(Card card)
     {
@@ -18,6 +23,30 @@ public class PlayerDeck : MonoBehaviour
         {
             deck.Remove(card);
             Debug.Log("Card Removed: " + card.id + ", " + card.CardName);
+        }
+    }
+    void Start()
+    {
+        warButton.interactable = false;
+    }
+    void Update()
+    {
+        size = deck.Count;
+        sizeText.text = "Grandezza Deck: " + size;
+        image.fillAmount = (float)(size * 0.1);
+        isInteractable(warButton);
+
+    } 
+    public void isInteractable (Button button)
+    {
+        if ((size == 10))
+        {
+            button.interactable = true; 
+        }
+        else
+        {
+            button.interactable = false; 
+
         }
     }
 
