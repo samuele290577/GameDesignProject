ef28f73251025d1c4724dbf4cf45a12adebd5da9
Gabriele Campanella <gabriele.campanella99@gmail.com>
2021-05-19 17:59:40 +0200

    Scelta Team Incomplete

---
+++ Game Design Project/Assets/ProvaSceltaTeam/Scripts/GoToDeck.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GoToDeck : MonoBehaviour
+{
+    public GameObject team1;
+    public string team1_name;
+    public string team2_name;
+    public GameObject team2;
+  
+    
+    public Button button; 
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        button.interactable = false;
+        team1_name = team1.GetComponent<TeamPickerManager>().chosenTeam;
+        team2_name = team2.GetComponent<TeamPickerManager>().chosenTeam;
+        Debug.Log(team1_name + " " + team2_name);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(team1_name + "ao" + team2_name);
+    }
+}
---
+++ Game Design Project/Assets/ProvaSceltaTeam/Scripts/TeamPickerManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TeamPickerManager : MonoBehaviour
+{
+    public Button button_plants;
+    public Button button_humans;
+    
+    public string chosenTeam;
+
+    public void ChosenPlants()
+    {
+        chosenTeam = "Plants";
+        Debug.Log(chosenTeam);
+        button_plants.image.color = Color.green;
+        button_humans.image.color = Color.white;
+    }
+    public void ChosenHumans()
+    {
+        chosenTeam = "Humans";
+        Debug.Log(chosenTeam);
+        button_humans.image.color = Color.red;
+        button_plants.image.color = Color.white;
+    }
+}
