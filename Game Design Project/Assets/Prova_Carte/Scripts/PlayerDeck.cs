b6fdfe3f3132da62af92f248eb9aab2047dcd4e1
Gabriele Campanella <gabriele.campanella99@gmail.com>
2021-05-28 00:24:50 +0200

    Passaggio scena con Deck Fatto

--- Game Design Project/Assets/Prova_Carte/Scripts/PlayerDeck.cs
+++ Game Design Project/Assets/Prova_Carte/Scripts/PlayerDeck.cs
@@ -5,16 +5,12 @@ using UnityEngine.UI;
 
 public class PlayerDeck : MonoBehaviour
 {
-    public List<Card> deck = new List<Card>();
-    public double size = 0;
-    public Text sizeText;
-    public Image image;
-    public Button warButton;
-    public int bananacounter = 0;
-    public int pinapplecounter = 0;
-    public int coconutcounter = 0;
-    public int watermeloncounter = 0;
-    public int sabbiacounter = 0;
+    static public List<Card> deck = new List<Card>();
+    private int bananacounter = 0;
+    private int pinapplecounter = 0;
+    private int coconutcounter = 0;
+    private int watermeloncounter = 0;
+    private int sabbiacounter = 0;
 
 
     public void AddCard(Card card)
@@ -107,6 +103,10 @@ public class PlayerDeck : MonoBehaviour
 
 
     }
+    public int getSize()
+    {
+        return deck.Count;
+    }
 
     public void RemoveCard(Card card)
     {
@@ -157,29 +157,7 @@ public class PlayerDeck : MonoBehaviour
             }
         }
     }
-    void Start()
-    {
-        warButton.interactable = false;
-    }
-    void Update()
-    {
-        size = deck.Count;
-        sizeText.text = "Grandezza Deck: " + size;
-        image.fillAmount = (float)(size * 0.1);
-        isInteractable(warButton);
-
-    }
-    public void isInteractable(Button button)
-    {
-        if ((size == 10))
-        {
-            button.interactable = true;
-        }
-        else
-        {
-            button.interactable = false;
-
-        }
-    }
+    
+    
 
 }
---
+++ Game Design Project/Assets/ShowDeck.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShowDeck : MonoBehaviour
+{
+   
+    public Text sizeText;
+    public PlayerDeck deck;
+    public Button warButton;
+    private int size;
+    public Image image;
+
+
+    void Start()
+    {
+        warButton.interactable = false;
+    }
+    void Update()
+    {
+        size = deck.getSize();
+        sizeText.text = "Grandezza deck: " + size;
+        image.fillAmount = (float)(size * 0.1);
+        isInteractable(warButton);
+
+    }
+    public void isInteractable(Button button)
+    {
+        if ((size == 10))
+        {
+            button.interactable = true;
+        }
+        else
+        {
+            button.interactable = false;
+
+        }
+    }
+}
