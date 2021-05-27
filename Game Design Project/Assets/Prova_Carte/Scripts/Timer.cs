f11f3a5957ffddc95c8f6f926846d0de6dc628a8
LAPTOP-T7FMJV41\aleti <ale.titanic@hotmail.it>
2021-05-27 11:54:44 +0200

    Prova Timer e Timer Events

---
+++ Game Design Project/Assets/Prova_Carte/Scripts/Timer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+    public float interval;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(interval > 0)
+        {
+            interval -= Time.deltaTime; 
+        }
+        else
+        {
+            enabled = false;
+        }
+    }
+}
