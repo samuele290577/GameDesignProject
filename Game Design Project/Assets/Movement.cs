32c4e963b954adceafbc9d446a341bd5bbf4bf80
Samuello <samuele.mannala@gmail.com>
2021-06-01 11:56:16 +0200

    Movement
    Movement on click

---
+++ Game Design Project/Assets/Movement.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Movement : MonoBehaviour
+{
+    NavMeshAgent agent;
+    public float rotateSpeedMovement = 0.1f;
+    float rotateVelocity;
+
+    void Start()
+    {
+        agent = gameObject.GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            RaycastHit hit;
+
+            //Check che stiamo hittando un navmesh system
+            if(Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, Mathf.Infinity))
+            {
+                //Player deve muoversi verso hit point
+                agent.SetDestination(hit.point);
+
+
+                //Rotazione
+                Quaternion rotationToLookAt = Quaternion.LookRotation(hit.point - transform.position);
+                float rotationY = Mathf.SmoothDampAngle(transform.eulerAngles.y,
+                    rotationToLookAt.eulerAngles.y,
+                    ref rotateVelocity,
+                    rotateSpeedMovement * (Time.deltaTime * 5));
+
+                transform.eulerAngles = new Vector3(0, rotationY, 0);
+                   
+            }
+        }
+
+
+
+
+
+
+    }
+}
