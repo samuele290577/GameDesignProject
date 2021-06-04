88b68e5fa2cced859f8a3c8d680e14a8aa210c83
Gabriele Campanella <gabriele.campanella99@gmail.com>
2021-06-04 12:18:28 +0200

    Split Screen e Deck Human

--- Game Design Project/Assets/ProvaSceltaTeam/Scripts/DeckManager.cs
+++ Game Design Project/Assets/ProvaSceltaTeam/Scripts/DeckManager.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class DeckManager : MonoBehaviour
 {
-    public PlayerDeck deck; 
+    public PlayerDeck deck_piante;
+    public PlayerDeck deck_umani;
+    
     void Start()
     {
-        Debug.Log("grandezza deck: " + deck.getSize());
+        Debug.Log("grandezza deck piante: " + deck_piante.getSize());
+        Debug.Log("grandezza deck umani: " + deck_umani.getSize());
     }
 
     // Update is called once per frame
