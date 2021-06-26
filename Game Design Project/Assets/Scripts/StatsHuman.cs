f37f418d4bb4ab4e20fa6f932d1abd109267ac76
Samuello <samuele.mannala@gmail.com>
2021-06-26 12:18:39 +0200

    Health system
    completato, bisogna solo mettere valori coerenti con documento

---
+++ Game Design Project/Assets/Scripts/StatsHuman.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StatsHuman : MonoBehaviour
+{
+    public int maxHealth = 1000;
+    public int currentHealth;
+    public HealthBar healthBar;
+    public Animator animator; 
+
+
+
+    void Start()
+    {
+        //Health set up on start
+        currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            TakeDamage(15);
+        }    
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        animator.SetTrigger("isHurt");
+        //apple 
+        if(collision.gameObject.name == "mela" || collision.gameObject.name == "mela(Clone)")
+        {
+            Debug.Log("apple collision detected");
+            TakeDamage(25);
+        }
+        //banana
+        else if (collision.gameObject.name == "banane" || collision.gameObject.name == "banane(Clone)")
+        {
+            Debug.Log("banana collision detected");
+            TakeDamage(50);
+        }
+        //pineapple
+        else if (collision.gameObject.name == "ananas" || collision.gameObject.name == "ananas(Clone)")
+        {
+            Debug.Log("ananas collision detected");
+            TakeDamage(25);
+        }
+        //coconut
+        else if (collision.gameObject.name == "cocco" || collision.gameObject.name == "cocco(Clone)")
+        {
+            Debug.Log("coconut collision detected");
+            TakeDamage(25);
+        }
+        //watermelon
+        else if (collision.gameObject.name == "anguria" || collision.gameObject.name == "anguria(Clone)")
+        {
+            Debug.Log("watermelon collision detected");
+            TakeDamage(25);
+        }
+        //arancia
+        else if(collision.gameObject.name == "arancia" || collision.gameObject.name == "arancia(Clone)")
+        {
+            Debug.Log("orange collision detected");
+            TakeDamage(25);
+        }
+    }
+    //function for damage
+    void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        healthBar.SetHealth(currentHealth);
+    }
+}
---
+++ Game Design Project/Assets/StatsPlant.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StatsPlant : MonoBehaviour
+{
+    public int maxHealth = 1000;
+    public int currentHealth;
+    public HealthBar healthBar;
+    //public Animator animator;
+
+    void Start()
+    {
+        //Health set up on start
+        currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            TakeDamagePlant(15);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        //animator.SetTrigger("isHurt");
+
+        //bomba 
+        if (collision.gameObject.name == "Bomba" || collision.gameObject.name == "Bomba(Clone)")
+        {
+            Debug.Log("bomba collision detected");
+            TakeDamagePlant(25);
+        }
+        //dinamite
+        else if (collision.gameObject.name == "dinamite" || collision.gameObject.name == "dinamite(Clone)")
+        {
+            Debug.Log("banana collision detected");
+            TakeDamagePlant(50);
+        }
+        //molotov
+        else if (collision.gameObject.name == "molotov" || collision.gameObject.name == "molotov(Clone)")
+        {
+            Debug.Log("ananas collision detected");
+            TakeDamagePlant(25);
+        }
+        //fucile a pompa
+        else if (collision.gameObject.name == "fucile a pompa" || collision.gameObject.name == "fucile a pompa(Clone)")
+        {
+            Debug.Log("coconut collision detected");
+            TakeDamagePlant(25);
+        }
+        //lanciarazzi
+        else if (collision.gameObject.name == "lancirazzi" || collision.gameObject.name == "lanciarazzi(Clone)")
+        {
+            Debug.Log("watermelon collision detected");
+            TakeDamagePlant(25);
+        }
+        //pugnale
+        else if (collision.gameObject.name == "pugnale" || collision.gameObject.name == "pugnale(Clone)")
+        {
+            Debug.Log("orange collision detected");
+            TakeDamagePlant(25);
+        }
+    }
+    //function for damage
+    void TakeDamagePlant(int damage)
+    {
+        currentHealth -= damage;
+        healthBar.SetHealth(currentHealth);
+    }
+}
+
+
