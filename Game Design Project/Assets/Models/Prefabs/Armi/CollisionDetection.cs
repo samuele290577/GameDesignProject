9aab15c1182e75a12a70b8a411c16c7fb9a7a755
marzio98 <marzio98@live.com>
2021-07-02 00:03:00 +0200

    Ostruzione powerup

--- Game Design Project/Assets/Models/Prefabs/Armi/CollisionDetection.cs
+++ Game Design Project/Assets/Models/Prefabs/Armi/CollisionDetection.cs
@@ -34,69 +34,80 @@ public class CollisionDetection : MonoBehaviour
             collision.gameObject.name == "Plant_Player" ||
             collision.gameObject.tag == "Obstacle" ||
             collision.gameObject.name == "Human_Player"
-            ) {
+            ) 
+        {
             Debug.Log("Collisione con " + collision.gameObject);
 
             //ARMI UMANO
 
             if (this.name == "Bomba" || this.name =="Bomba(Clone)")
-        {
-            Debug.Log("la bomba!");
-            TakeDamage(25);
-        }
-        else if (this.name == "Dinamite" || this.name == "Dinamite(Clone)")
-        {
-            Debug.Log("la dinamite!");
-            TakeDamage(15);
-        }
-        else if (this.name == "Molotov" || this.name == "Molotov(Clone)")
-        {
-            Debug.Log("la molotov!");
-            TakeDamage(35);
-        }
-        else if (this.name == "projectile" || this.name == "projectile(Clone)")
-        {
-            Debug.Log("fucile a pompa o lancia razzi");
-            TakeDamage(20);
-        }
+            {
+                Debug.Log("la bomba!");
+                if (checkObstructionPower("Humans")) TakeDamage((int)(25 * 1.5));
+                else TakeDamage(25);
+                }
+            else if (this.name == "Dinamite" || this.name == "Dinamite(Clone)")
+            {
+                Debug.Log("la dinamite!");
+                if (checkObstructionPower("Humans")) TakeDamage((int)(15 * 1.5));
+                else TakeDamage(15);
+            }
+            else if (this.name == "Molotov" || this.name == "Molotov(Clone)")
+            {
+                Debug.Log("la molotov!");
+                if (checkObstructionPower("Humans")) TakeDamage((int)(35 * 1.5));
+                else TakeDamage(35);
+            }
+            else if (this.name == "projectile" || this.name == "projectile(Clone)")
+            {
+                if (checkObstructionPower("Humans")) TakeDamage((int)(20 * 1.5));
+                else TakeDamage(20);
+            }
   
-        else if (this.name == "Pugnale" || this.name == "Pugnale(Clone)")
-        {
-            Debug.Log("il pugnale!");
-            TakeDamage(30);
-        }
+            else if (this.name == "Pugnale" || this.name == "Pugnale(Clone)")
+            {
+                Debug.Log("il pugnale!");
+                if (checkObstructionPower("Humans")) TakeDamage((int)(30 * 1.5));
+                else TakeDamage(30);
+            }
 
-        //ARMI PIANTE
+            //ARMI PIANTE
 
-        else if(this.name =="mela" || this.name == "mela(Clone)")
+            else if(this.name =="mela" || this.name == "mela(Clone)")
             {
                 Debug.Log("mela");
-                TakeDamage(25);
+                if (checkObstructionPower("Plants")) TakeDamage((int)(25 * 1.5));
+                else TakeDamage(25);
             }
             else if (this.name == "banana" || this.name == "banana(Clone)")
             {
                 Debug.Log("banana");
-                TakeDamage(15);
+                if (checkObstructionPower("Plants")) TakeDamage((int)(15 * 1.5));
+                else TakeDamage(15);
             }
             else if (this.name == "ananas" || this.name == "ananas(Clone)")
             {
                 Debug.Log("ananas");
-                TakeDamage(35);
+                if (checkObstructionPower("Plants")) TakeDamage((int)(35 * 1.5));
+                else TakeDamage(35);
             }
             else if (this.name == "cocco" || this.name == "cocco(Clone)")
             {
                 Debug.Log("cocco");
-                TakeDamage(25);
+                if (checkObstructionPower("Plants")) TakeDamage((int)(25 * 1.5));
+                else TakeDamage(25);
             }
             else if (this.name == "anguria" || this.name == "anguria(Clone)")
             {
-                Debug.Log("anguria");  
-                TakeDamage(20);
+                Debug.Log("anguria");
+                if (checkObstructionPower("Plants")) TakeDamage((int)(20 * 1.5));
+                else TakeDamage(20);
             }
             else if (this.name == "arancia" || this.name == "arancia(Clone)")
             {
                 Debug.Log("arancia");
-                TakeDamage(30);
+                if (checkObstructionPower("Plants")) TakeDamage((int)(30 * 1.5));
+                else TakeDamage(30);
             }
 
 
@@ -108,6 +119,7 @@ public class CollisionDetection : MonoBehaviour
 
     void TakeDamage(int damage)
     {
+        Debug.Log("Danno inflitto: " + damage);
         foreach(var t in targets)
         {
             //diventerà stats
@@ -117,6 +129,33 @@ public class CollisionDetection : MonoBehaviour
         }
     }
 
+    bool checkObstructionPower(string team)
+    {
+        if (team == "Plants")
+        {
+            var ostruzioni = FindObjectsOfType<PhysicalOstruzione>();
+            foreach (var el in ostruzioni)
+            {
+                if (el.id == 7)
+                {
+                    if (Vector3.Distance(transform.position, el.gameObject.transform.position) < 5 ) return true;
+                }
+            }
 
+        }
+        else if (team == "Humans")
+        {
+            var ostruzioni = FindObjectsOfType<PhysicalOstruzione>();
+            foreach (var el in ostruzioni)
+            {
+                if (el.id == 17)
+                {
+                    if (Vector3.Distance(transform.position, el.gameObject.transform.position) < 5) return true;
+                }
+            }
+
+        }
+        return false;
+    }
 
 }
