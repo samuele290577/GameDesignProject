79281f646cd1ee5e0ec71bba1b6354b7e4fd361a
Samuello <samuele.mannala@gmail.com>
2021-07-04 22:04:45 +0200

    Improvements collision detection script

--- Game Design Project/Assets/Models/Prefabs/Armi/CollisionDetection.cs
+++ Game Design Project/Assets/Models/Prefabs/Armi/CollisionDetection.cs
@@ -8,6 +8,8 @@ public class CollisionDetection : MonoBehaviour
     public GameObject DamageArea;
     public GameObject arma;
     public List<GameObject> targets = new List<GameObject>();
+    public GameObject earthLogic;
+    public GameObject currentPlayer;
   
   
     void Start()
@@ -24,6 +26,7 @@ public class CollisionDetection : MonoBehaviour
 
     private void Update()
     {
+        earthLogic = GameObject.Find("GameLogic");
         targets = DamageArea.GetComponent<DamageArea>().potentialTargets;
     }
 
@@ -41,99 +44,171 @@ public class CollisionDetection : MonoBehaviour
             {
                 Debug.Log(gameObject + "Debug lancio:" + t);
             }
-        
+
         {
-            
+
 
             //ARMI UMANO
 
-            if (this.name == "Bomba" || this.name =="Bomba(Clone)")
+            if (this.name == "Bomba" || this.name == "Bomba(Clone)")
             {
-                Debug.Log("la bomba!");
-                if (checkObstructionPower("Humans")) TakeDamage((int)(25 * 1.5));
-                else TakeDamage(25);
+                foreach (var t in targets)
+                {
+                    if (t.gameObject.name != "Human_Player")
+                    {
+                        Debug.Log("bomba");
+                        if (checkObstructionPower("Humans")) TakeDamage(t, (int)(25 * 1.5));
+                        else TakeDamage(t, 25);
+                    }
                 }
+            }
+
             else if (this.name == "Dinamite" || this.name == "Dinamite(Clone)")
             {
-                Debug.Log("la dinamite!");
-                if (checkObstructionPower("Humans")) TakeDamage((int)(15 * 1.5));
-                else TakeDamage(15);
+                foreach (var t in targets)
+                {
+                    if (t.name != "Human_Player")
+                    {
+                        Debug.Log("dinamite");
+                        if (checkObstructionPower("Humans")) TakeDamage(t, (int)(15 * 1.5));
+                        else TakeDamage(t, 15);
+                    }
+                }
             }
             else if (this.name == "Molotov" || this.name == "Molotov(Clone)")
             {
-                Debug.Log("la molotov!");
-                if (checkObstructionPower("Humans")) TakeDamage((int)(35 * 1.5));
-                else TakeDamage(35);
+                foreach (var t in targets)
+                {
+                    if (t.name != "Human_Player")
+                    {
+                        Debug.Log("molotov");
+                        if (checkObstructionPower("Humans")) TakeDamage(t, (int)(35 * 1.5));
+                        else TakeDamage(t, 35);
+                    }
+                }
+
             }
             else if (this.name == "projectile" || this.name == "projectile(Clone)")
             {
-                if (checkObstructionPower("Humans")) TakeDamage((int)(20 * 1.5));
-                else TakeDamage(20);
+                currentPlayer = earthLogic.GetComponent<Logic_Earth>().currentPlayer;
+                currentPlayer.GetComponent<BoxCollider>().enabled = false;
+                 
+                foreach (var t in targets)
+                {
+                    if (t.name != "Human_Player")
+                    {
+                        Debug.Log("projectile");
+                        if (checkObstructionPower("Humans")) TakeDamage(t, (int)(20 * 1.5));
+                        else TakeDamage(t, 20);
+                    }
+                }
+                currentPlayer.GetComponent<BoxCollider>().enabled = true;
             }
-  
+
             else if (this.name == "Pugnale" || this.name == "Pugnale(Clone)")
             {
-                Debug.Log("il pugnale!");
-                if (checkObstructionPower("Humans")) TakeDamage((int)(30 * 1.5));
-                else TakeDamage(30);
+                foreach (var t in targets)
+                {
+                    if (t.name != "Human_Player")
+                    {
+                        Debug.Log("pugnale");
+                        if (checkObstructionPower("Humans")) TakeDamage(t, (int)(30 * 1.5));
+                        else TakeDamage(t, 30);
+                    }
+                }
             }
 
             //ARMI PIANTE
 
-            else if(this.name =="mela" || this.name == "mela(Clone)")
+            else if (this.name == "mela" || this.name == "mela(Clone)")
             {
-                Debug.Log("mela");
-                if (checkObstructionPower("Plants")) TakeDamage((int)(25 * 1.5));
-                else TakeDamage(25);
+                foreach (var t in targets)
+                {
+                    if (t.name != "Plant_Player")
+                    {
+                        Debug.Log("mela");
+                        if (checkObstructionPower("Plants")) TakeDamage(t, (int)(25 * 1.5));
+                        else TakeDamage(t, 25);
+                    }
+                }
+
             }
             else if (this.name == "banane" || this.name == "banane(Clone)")
             {
-                Debug.Log("banana");
-                if (checkObstructionPower("Plants")) TakeDamage((int)(15 * 1.5));
-                else TakeDamage(15);
+                foreach (var t in targets)
+                {
+                    if (t.name != "Plant_Player")
+                    {
+                        Debug.Log("banane");
+                        if (checkObstructionPower("Plants")) TakeDamage(t, (int)(15 * 1.5));
+                        else TakeDamage(t, 15);
+                    }
+                }
             }
+
             else if (this.name == "ananas" || this.name == "ananas(Clone)")
             {
-                Debug.Log("ananas");
-                if (checkObstructionPower("Plants")) TakeDamage((int)(35 * 1.5));
-                else TakeDamage(35);
+                foreach (var t in targets)
+                {
+                    if (t.name != "Plant_Player")
+                    {
+                        Debug.Log("ananas");
+                        if (checkObstructionPower("Plants")) TakeDamage(t, (int)(35 * 1.5));
+                        else TakeDamage(t, 35);
+                    }
+                }
             }
+
             else if (this.name == "cocco" || this.name == "cocco(Clone)")
             {
-                Debug.Log("cocco");
-                if (checkObstructionPower("Plants")) TakeDamage((int)(25 * 1.5));
-                else TakeDamage(25);
+                foreach (var t in targets)
+                {
+                    if(t.name != "Plant_Player")
+                    {
+                        Debug.Log("cocco");
+                        if (checkObstructionPower("Plants")) TakeDamage(t, (int)(25 * 1.5));
+                        else TakeDamage(t, 25);
+                    }
+                }
+
+       
             }
             else if (this.name == "anguria" || this.name == "anguria(Clone)")
             {
-                Debug.Log("anguria");
-                if (checkObstructionPower("Plants")) TakeDamage((int)(20 * 1.5));
-                else TakeDamage(20);
+                foreach (var t in targets)
+                {
+                    if (t.name != "Plant_Player")
+                    {
+                        Debug.Log("anguria");
+                        if (checkObstructionPower("Plants")) TakeDamage(t, (int)(20 * 1.5));
+                        else TakeDamage(t, 20);
+                    }
+                }
             }
             else if (this.name == "arancia" || this.name == "arancia(Clone)")
             {
-                Debug.Log("arancia");
-                if (checkObstructionPower("Plants")) TakeDamage((int)(30 * 1.5));
-                else TakeDamage(30);
+                foreach (var t in targets)
+                {
+                    if (t.name != "Plant_Player")
+                    {
+                        Debug.Log("arancia");
+                        if (checkObstructionPower("Plants")) TakeDamage(t, (int)(30 * 1.5));
+                        else TakeDamage(t, 30);
+                    }
+                }
             }
+                Destroy(gameObject);
 
-
-            Destroy(gameObject);
-
-        }
+            }
+        
         
     }
 
-    void TakeDamage(int damage)
-    {
-        Debug.Log("Danno inflitto: " + damage);
-        foreach(var t in targets)
-        {
-            //diventerà stats
+    void TakeDamage(GameObject t, int damage)
+    { 
             t.GetComponent<Stats>().currentHealth -= damage;
             int newCurrentHealth = t.GetComponent<Stats>().currentHealth;
             t.GetComponent<Stats>().healthBar.SetHealth(newCurrentHealth);
-        }
     }
 
     bool checkObstructionPower(string team)
