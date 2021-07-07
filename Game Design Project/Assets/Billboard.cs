e6853d19de9b99e2beca185a42f38a8c74851b4b
Samuello <samuele.mannala@gmail.com>
2021-07-07 12:14:34 +0200

    Inizio Health System Ostruzioni (muro)

---
+++ Game Design Project/Assets/Billboard.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Billboard : MonoBehaviour
+{
+    public Transform cam;
+    private void Start()
+    {
+        cam = GameObject.Find("Main Camera").transform;
+    }
+
+    void Update()
+    {
+        transform.LookAt(transform.position + cam.forward);   
+    }
+}
