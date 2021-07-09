a31be125b3074d9d86b33211fcd59e0f313743e6
LAPTOP-T7FMJV41\aleti <ale.titanic@hotmail.it>
2021-07-09 12:18:54 +0200

    Scripts per i video

---
+++ Game Design Project/Assets/Scripts/Scriptsforstories/humancredits.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class humancredits : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(CreditsCoroutine());
+    }
+
+
+    IEnumerator CreditsCoroutine()
+    {
+
+        yield return new WaitForSeconds(24);
+
+        SceneManager.LoadScene("Scena_credits");
+    }
+}
