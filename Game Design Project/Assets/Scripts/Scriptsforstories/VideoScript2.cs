7a2c4a927ab7718c8906e01dcd9910453740f763
LAPTOP-T7FMJV41\aleti <ale.titanic@hotmail.it>
2021-07-09 16:56:17 +0200

    Video collegati

---
+++ Game Design Project/Assets/Scripts/Scriptsforstories/VideoScript2.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NDream.AirConsole;
+using UnityEngine.Video;
+using UnityEngine.SceneManagement;
+
+public class VideoScript2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        AirConsole.instance.Broadcast(new { action = "showTeaser" });
+
+
+        GameObject camera = GameObject.Find("Main Camera");
+        var videoPlayer = camera.AddComponent<VideoPlayer>();
+        videoPlayer.playOnAwake = false;
+        videoPlayer.url = System.IO.Path.Combine(Application.streamingAssetsPath, "teaserdagioco.mp4");
+        videoPlayer.Play();
+        videoPlayer.loopPointReached += CheckOver;
+    }
+
+    void CheckOver(UnityEngine.Video.VideoPlayer vp)
+    {
+        SceneManager.LoadScene("SceltaTeam");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SceneManager.LoadScene("SceltaTeam");
+        }
+    }
+}
---
+++ Game Design Project/Assets/Scripts/Scriptsforstories/VideoScript6.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NDream.AirConsole;
+using UnityEngine.Video;
+using UnityEngine.SceneManagement;
+
+public class VideoScript6 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        AirConsole.instance.Broadcast(new { action = "showCredits" });
+
+
+        GameObject camera = GameObject.Find("Main Camera");
+        var videoPlayer = camera.AddComponent<VideoPlayer>();
+        videoPlayer.playOnAwake = false;
+        videoPlayer.url = System.IO.Path.Combine(Application.streamingAssetsPath, "rolling credits.mp4");
+        videoPlayer.Play();
+        videoPlayer.loopPointReached += CheckOver;
+    }
+
+    void CheckOver(UnityEngine.Video.VideoPlayer vp)
+    {
+        Application.Quit();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Application.Quit();
+        }
+    }
+}
