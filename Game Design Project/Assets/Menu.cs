ce4edb4f11fd27c54914aa6c96f81b726b9806a0
LAPTOP-T7FMJV41\aleti <ale.titanic@hotmail.it>
2021-07-09 21:53:58 +0200

    Ultime modifiche

---
+++ Game Design Project/Assets/Menu.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using NDream.AirConsole;
+using Newtonsoft.Json.Linq;
+
+public class Menu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Scena_teaser");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void HelpGame()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    void Awake()
+    {
+        AirConsole.instance.onMessage += OnMessage;
+    }
+
+    void OnMessage(int fromDeviceID, JToken data)
+    {
+        if (data["action"] != null && data["action"].ToString() == "play")
+        {
+            SceneManager.LoadScene("Scena_teaser");
+        }
+        else if (data["action"] != null && data["action"].ToString() == "quit")
+        {
+            Application.Quit();
+        }
+        else if (data["action"] != null && data["action"].ToString() == "tutorial")
+        {
+            SceneManager.LoadScene("MainMenu");
+        }
+    }
+}
